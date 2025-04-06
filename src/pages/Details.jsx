f7655f91e3643302f4a37e816c6df15534937c2f
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-04-06 17:46:52 +0200

    Enhance Details page layout; update styles for big_card and big-img for improved presentation

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,25 +1,25 @@
 import { useGlobal } from "../contexts/GlobalContext";
 
 export default function Details() {
-    const { details } = useGlobal()
+    const { details } = useGlobal();
     return (
         <>
-            <div className="container rounded big_card">
-
-                <div className="card text-bg-dark asd">
-                    <img src={`http://image.tmdb.org/t/p/w1920/${details.img}`} alt={details.title} className="card-img" />
+            <div className="container rounded big_card bg-dark">
+                <div className="card text-bg-dark">
+                    <img
+                        src={`http://image.tmdb.org/t/p/w1920/${details.img}`}
+                        alt={details.title}
+                        className="big-img" // Ensure the class is applied
+                    />
                     <div className="card-img-overlay">
-                        <h5 className="card-title  w-25 ">{details.title}</h5>
-
+                        <h5 className="card-title w-25">{details.title}</h5>
                     </div>
                 </div>
             </div>
-            <div className="container rounded big_card">
-
-                <div className="card text-bg-dark asd">
-                    <div className="card-img-overlay">
-                        <p className="card-text ">{details.description}</p>
-                    </div>
+            <div className="container rounded big_card bg-dark">
+                <div className="card bg-dark  mt-4">
+                    <h4 className="text-white mt-2 ">Description:</h4>
+                    <p className="card-text text-white mt-2 mb-4">{details.description}</p>
                 </div>
             </div>
         </>
