a7a114584941adcb0f589ceb17025cd4a38eb067
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-04-07 22:36:26 +0200

    Refactor Search component; remove unused genre selection and enhance video trailer functionality

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,26 +2,12 @@ import { useGlobal } from "../contexts/GlobalContext"
 import Card from "../components/CardFilm"
 
 export default function Search() {
-    const { searchResults, languageCode, rowRef, slide, takefilm, genres, getVideo } = useGlobal()
+    const { searchResults, languageCode, takefilm, getVideo } = useGlobal()
 
     return (
         <>
             <main>
-                <div className="m-5 p-4">
-                    <div class="mb-3">
-                        <select
-                            class=" form-select-sm"
-                            name=""
-                            id=""
-                        >
-                            <option selected>Select one</option>
-                            {genres.map(genres => {
-                                return (
-                                    <option value={genres.id}>{genres.name}</option>
-                                )
-                            })}
-                        </select>
-                    </div>
+                {searchResults === "" ? "Cerca film" :
 
                     <div className="search-results">
 
@@ -43,13 +29,16 @@ export default function Search() {
                                         lang={languageCode}
                                         stars={getStars()}
                                         takefilm={() => takefilm(item)}
+                                        onClick={() => getVideo(item)} // Update trailer on click
+
                                     />
                                 );
                             })}
                         </div>
                     </div>
-                </div>
-            </main>
+                }
+
+            </main >
         </>
     )
 }
